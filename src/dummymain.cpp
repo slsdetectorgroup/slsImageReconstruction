5e579d96fc1fbfbd07ef8c161d54c224c06da75c
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2015-09-23 17:00:18 +0200

    more refactoring in dummymain

--- src/dummymain.cpp
+++ src/dummymain.cpp
@@ -19,16 +19,11 @@
 
 using namespace std;
 
-int getParameters(int argc, char *argv[], int &dr, string &fntop, string &fnbottom, int &tg);
+int getParameters(int argc, char *argv[], string &file, int &bottom, int &tg);
 
 
 int main(int argc, char *argv[]) {
-	//declare variables
-	slsReceiverData <uint32_t> *receiverdata = NULL;
-	char* buffer = NULL;
-	ifstream infile;
-	string fnametop = "", fnamebottom = "";
-	int dynamicrange, tenGiga, bufferSize, dataSize, packetsPerFrame, ix, iy, numFrames, fnum;
+
 	/** structure of an eiger packet*/
 	typedef struct
 	{
@@ -45,53 +40,34 @@ int main(int argc, char *argv[]) {
 	} eiger_packet_footer_t;
 
 
+	//declare variables
+	slsReceiverData <uint32_t> *receiverdata = NULL;
+	char* buffer = NULL;
+	ifstream infile;
+	string file = "";
+	int dynamicrange, tenGiga, bufferSize, dataSize, packetsPerFrame, ix, iy, numFrames, fnum, bottom;
+
 	//get config parameters
-	dynamicrange = -1;
 	tenGiga = 0;
-	if(getParameters(argc, argv, dynamicrange,fnametop,fnamebottom, tenGiga) == slsReceiverDefs::OK){
-		//if no dynamic range given
-		if(dynamicrange == -1){
-			cout << "Getting Dynamic Range from File..." << endl;
-			char *data = new char[1024];
-			int dynamicrange2=-1;
+	if(getParameters(argc,argv,file,bottom,tenGiga) == slsReceiverDefs::OK){
 
-			//top dynamic range
-			if(!fnametop.empty()){
-				infile.open(fnametop.c_str(),ios::in | ios::binary);
-				if (infile.is_open()) {
-					if(infile.read(data,1024)){
-						dynamicrange = eigerHalfModuleData::getDynamicRange(data);
-					}
-					infile.close();
-				}else{
-					cprintf(RED, "Error: Could not read top file: %s\n", fnametop.c_str());
-					delete [] data;
-					exit(-1);
+		cout << "Getting Dynamic Range from File..." << endl;
+		char *data = new char[1024];
+		dynamicrange = -1;
+		if(!file.empty()){
+			infile.open(file.c_str(),ios::in | ios::binary);
+			if (infile.is_open()) {
+				if(infile.read(data,1024)){
+					dynamicrange = eigerHalfModuleData::getDynamicRange(data);
 				}
+				infile.close();
+			}else{
+				cprintf(RED, "Error: Could not read file: %s\n", file.c_str());
+				delete [] data;
+				exit(-1);
 			}
-
-			//bottom dynamic range
-			if(!fnamebottom.empty()){
-				infile.open(fnamebottom.c_str(),ios::in | ios::binary);
-				if (infile.is_open()) {
-					if(infile.read(data,1024)){
-						dynamicrange2 = eigerHalfModuleData::getDynamicRange(data);
-						if ((dynamicrange != -1) && (dynamicrange != dynamicrange2)){
-							cprintf(RED, "Error: Dynamic range of top %d and bottom %d does not match\n", dynamicrange,dynamicrange2);
-							exit(-1);
-						}
-						else dynamicrange = dynamicrange2; //only bottom checked
-					}
-					infile.close();
-				}else{
-					cprintf(RED, "Error: Could not read bottom file: %s\n", fnamebottom.c_str());
-					delete [] data;
-					exit(-1);
-				}
-			}
-
-			delete [] data;
 		}
+		delete [] data;
 
 		//verify dynamic range
 		switch(dynamicrange){
@@ -103,11 +79,15 @@ int main(int argc, char *argv[]) {
 			cprintf(RED, "Error: Invalid dynamic range %d. Options: 4,8,16,32.\n", dynamicrange);
 			exit(-1);
 		}
-		cout << "\n\nDynamic range:"<< dynamicrange << "\nTop File name:" << fnametop << "\nBottom File name:" << fnamebottom << "\nTen giga:"<< tenGiga << endl << endl;
+		cout << "\n\nDynamic range:"<< dynamicrange <<
+				"\nFile name:" << file <<
+				"\nTen giga:" << tenGiga << "\nHalfMoule:";
+		if(bottom)	cout << "bottom";	else cout << "top";
+		cout << endl << endl;
 
 		//initialize variables for 1g and 10g
 		ix=0;
-		iy = 0;
+		iy=0;
 		bufferSize = 1040;
 		dataSize = 1024;
 		packetsPerFrame = 16 * dynamicrange * 2;
@@ -117,23 +97,20 @@ int main(int argc, char *argv[]) {
 			dataSize = 4096;
 		}
 
-		//top
-		if(!fnametop.empty()){
+		//read values
+		if(!file.empty()){
 			//construct top datamapping object
 			numFrames = 0;
 			fnum = -1;
-			receiverdata = new eigerHalfModuleData(dynamicrange,packetsPerFrame,bufferSize, dataSize, true);
+			receiverdata = new eigerHalfModuleData(dynamicrange,packetsPerFrame,bufferSize, dataSize, (bottom?0:1));
 			//open file
-			infile.open(fnametop.c_str(),ios::in | ios::binary);
+			infile.open(file.c_str(),ios::in | ios::binary);
 			if(infile.is_open()){
 				//get frame buffer
 				while((buffer = receiverdata->readNextFrame(infile, fnum))){
 					//while((buffer = receiverdata->readNextFrame(infile))){
-					cout << "Reading top values for frame #" << fnum << endl;
+					cout << "Reading values for frame #" << fnum << endl;
 					//getting values
-
-
-
 
 
 					for(iy = 0; iy < 5; iy++){
@@ -143,58 +120,16 @@ int main(int argc, char *argv[]) {
 							cprintf(BLUE,"%d,%d :%f\n",ix,iy,(receiverdata->getValue((char*)buffer,ix,iy,dynamicrange)));
 					}
 
-
-
-
 					delete [] buffer;
 					numFrames++;
 				}
 				//close file
 				infile.close();
-			}else cprintf(RED, "Error: Could not read top file: %s\n", fnametop.c_str());
+			}else cprintf(RED, "Error: Could not read file: %s\n", file.c_str());
 			delete receiverdata;
-			cout  << "Found " << numFrames << " frames in top file." << endl << endl;
+			cout  << "Found " << numFrames << " frames in file." << endl << endl;
 		}
-		//bottom
-		if(!fnamebottom.empty()){
-			//construct bottom datamapping object
-			numFrames = 0;
-			fnum = -1;
-			receiverdata = new eigerHalfModuleData(dynamicrange,packetsPerFrame,bufferSize, dataSize, false);
-			//open file
-			infile.open(fnamebottom.c_str(),ios::in | ios::binary);
-			if(infile.is_open()){
-				//get frame buffer
-				while((buffer = receiverdata->readNextFrame(infile,fnum))){
-					//while((buffer = receiverdata->readNextFrame(infile))){
-					cout << "Reading bottom values for frame #" << fnum << endl;
 
-					//getting values
-
-
-
-
-					for(iy = 0; iy < 3; iy++){
-						for(ix = 0; ix < 2; ix++)
-							cprintf(BLUE,"%d,%d :%f\n",ix,iy,(receiverdata->getValue((char*)buffer,ix,iy,dynamicrange)));
-						for(ix = 254; ix < 258; ix++)
-							cprintf(BLUE,"%d,%d :%f\n",ix,iy,(receiverdata->getValue((char*)buffer,ix,iy,dynamicrange)));
-					}
-
-
-
-
-
-					delete [] buffer;
-					numFrames++;
-				}
-
-				//close file
-				infile.close();
-			}else cprintf(RED, "Error: Could not read bottom file: %s\n", fnamebottom.c_str());
-			delete receiverdata;
-			cout  << "Found " << numFrames << " frames in bottom file." << endl;
-		}
 	}
 	cout << endl << "Goodbye!" << endl;
 	return slsReceiverDefs::OK;
@@ -205,11 +140,9 @@ int main(int argc, char *argv[]) {
 /**
  * Parse command line parameters to get dynamic range, ten giga enable and file names
  */
-int getParameters(int argc, char *argv[], int &dr, string &fntop, string &fnbottom,int &tg){
+int getParameters(int argc, char *argv[], string &file, int &bottom,int &tg){
 
 	map<string, string> configuration_map;
-	string file;
-	int top;
 	size_t detPosition,framePosition,filePosition,endPosition;
 
 	static struct option long_options[] = {
@@ -221,6 +154,10 @@ int getParameters(int argc, char *argv[], int &dr, string &fntop, string &fnbott
 
 	int option_index = 0, c;
 
+	if(argc<3){
+		cprintf(RED,"ERROR: Invalid arguments. image --file [file]\n");
+		return slsReceiverDefs::FAIL;
+	}
 
 	while ( c != -1 ){
 		c = getopt_long (argc, argv, "tfh", long_options, &option_index);
@@ -258,29 +195,23 @@ int getParameters(int argc, char *argv[], int &dr, string &fntop, string &fnbott
 				endPosition = framePosition;
 				if(framePosition == string::npos)
 					endPosition = filePosition;
-				if (!sscanf(file.substr(detPosition+1,endPosition-1-detPosition).c_str(),"d%d",&top)) {
+				if (!sscanf(file.substr(detPosition+1,endPosition-1-detPosition).c_str(),"d%d",&bottom)) {
 					cprintf(RED, "Error: Invalid file name. Cannot parse detector index from %s\n",file.c_str());
 					return slsReceiverDefs::FAIL;
 				}
-				if(top%2)
-					fnbottom = file;
-				else
-					fntop = file;
 
+				bottom%=2;
 				break;
 			case 'h':
 				string help_message = """\nSLS Image Reconstruction\n\n""";
-				help_message += """usage: image --p top_fname [--m bottom_fname] [--dr dynamic_range]\n\n""";
-				help_message += """\t--dr:\t dynamic range or bit mode. Default:16. Options: 4,8,16,32.\n""";
+				help_message += """usage: image [--f file_name] [--t ten_giga_enable]\n\n""";
 				help_message += """\t--tengiga:\t 1 if 10Gbe or 0 for 1Gbe. Default: 0\n""";
-				help_message += """\t--top:\t file name of top image\n""";
-				help_message += """\t--bottom:\t file name of bottom image\n\n""";
+				help_message += """\t--file:\t file name of any raw file\n""";
 				cout << help_message << endl;
 				break;
 
 		}
 	}
 
-	//cout << " dynamic range:"<< dr << " top file name:" << fntop << " bottom file name:" << fnbottom << " ten giga:"<< tg << endl;
 	return slsReceiverDefs::OK;
 }
